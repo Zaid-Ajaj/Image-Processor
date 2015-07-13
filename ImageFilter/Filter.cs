bc88a78f0d70c0362ee7d255bc59e8d043afd9f4
Zaid-Ajaj <zaid.naom@gmail.com>
2015-07-13 23:59:46 +0200

    Added comments

--- ImageFilter/Filter.cs
+++ ImageFilter/Filter.cs
@@ -4,9 +4,10 @@ using static System.Math;
 
 namespace ImageFilter
 {
-    // filters a bitmap 
+    // filters a bitmap, highly effecient
     public class Filter
     {
+        // the linear filters are just functions f: Color -> Color to process every pixel in parallel
         public static Bitmap GrayScale(Bitmap source) => 
             source.LinearFilter(input => {
                 var avg = (input.R + input.G + input.B)/3;
@@ -15,7 +16,10 @@ namespace ImageFilter
 
         public static Bitmap Invert(Bitmap source) =>
             source.LinearFilter(input => {
-                return Color.FromArgb(255 - input.R, 255 - input.G, 255 - input.B);
+                var red = 255 - input.R;
+                var green = 255 - input.G;
+                var blue = 255 - input.B;
+                return Color.FromArgb(red, green, blue);
             });
 
         public static Bitmap SepiaTone(Bitmap source) =>
@@ -44,6 +48,7 @@ namespace ImageFilter
 
         public static Bitmap DetectEdges(Bitmap source, int threshold)
         {
+            // needs an unchanged copy of itself for reference
             var input = new LockBitmap(source);
             var output = new Bitmap(source.Width, source.Height);
             var bmpOutput = new LockBitmap(output);
@@ -53,6 +58,7 @@ namespace ImageFilter
             {
                 Parallel.For(1, input.Width - 1, x =>
                 {
+                    // taking input from the unchanged copy
                     var nextY = input.GetPixel(x, y + 1);
                     var prevY = input.GetPixel(x, y - 1);
                     var nextX = input.GetPixel(x + 1, y);
@@ -61,6 +67,7 @@ namespace ImageFilter
                     int yRed = nextY.R - prevY.R;
                     double result = Min(0.5 * Sqrt(xRed * xRed  + yRed * yRed), 255);
                     if (threshold > result)
+                        // output in another object
                         bmpOutput.SetPixel(x, y, Color.White);
                     else
                         bmpOutput.SetPixel(x, y, Color.Black);
